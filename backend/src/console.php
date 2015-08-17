ebfa52acc674825d5c1e2a10ffbc9991ecead8ac
Petru <petru.office92@gmail.com>
2015-08-17 16:03:38 +0200

    Optimized repo:* commands and beautified repo:version command.

--- backend/src/console.php
+++ backend/src/console.php
@@ -9,49 +9,42 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use GitWrapper\GitWrapper;
 
-$cmsinfo = [
-  'repository' => rtrim(file_get_contents(CMS_SAFELOCKER.'/release')),
-  'version' => rtrim(file_get_contents(CMS_SAFELOCKER.'/hacktor/release.log')),
-];
-
-$console = new Application('Metin2CMS', sprintf('%s::%s', $cmsinfo['repository'], $cmsinfo['version']));
+$console = new Application('Metin2CMS', rtrim(file_get_contents(CMS_SAFELOCKER.'/hacktor/release.log')));
 $console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
 $console->setDispatcher($app['dispatcher']);
 
 $console
-  ->register('repo:git-version')
-  ->setDefinition(array(
-    // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
-  ))
-  ->setDescription('Display repository version from Git.')
-  ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-    $repository = rtrim(file_get_contents(CMS_SAFELOCKER.'/release'));
-
-    $wrapper = new GitWrapper();
-    $wrapper->setTimeout(3600);
-    $git = $wrapper->workingCopy(CMS_ROOT);
-
-    if ($repository == 'nightly') {
-      $version = ltrim($wrapper->git('rev-parse --short HEAD'));
-    } else if($repository == 'stable') {
-      $version = ltrim($wrapper->git('describe --exact-match --abbrev=0'));
-    } else {
-      $version = 'unknown';
-    }
-
-    $output->writeln(sprintf('Version <info>%s</info> on repo <info>%s</info>', $version, $repository));
-  });
-$console
   ->register('repo:version')
   ->setDefinition(array(
-    // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
+    new InputOption('source', 's', InputOption::VALUE_REQUIRED, 'The source from where version is displayed. Defaults to cache.'),
   ))
-  ->setDescription('Display repository version from cache.')
+  ->setDescription('Display Metin2CMS version.')
   ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-    $version = rtrim(file_get_contents(CMS_SAFELOCKER.'/hacktor/release.log'));
-    $data = explode('::', $version);
+    $source = $input->getOption('source');
+    if ($source == 'git') {
+      $repository = rtrim(file_get_contents(CMS_SAFELOCKER.'/release'));
 
-    $output->writeln(sprintf('Version <info>%s</info> on repo <info>%s</info>', $data[1], $data[0]));
+      $wrapper = new GitWrapper();
+      $wrapper->setTimeout(3600);
+      $git = $wrapper->workingCopy(CMS_ROOT);
+
+      if ($repository == 'nightly') {
+        $version = ltrim($wrapper->git('rev-parse --short HEAD'));
+      } else if($repository == 'stable') {
+        $version = ltrim($wrapper->git('describe --exact-match --abbrev=0'));
+      } else {
+        $version = 'unknown';
+      }
+    } else if ($source == 'cache') {
+      $data = explode('::', rtrim(file_get_contents(CMS_SAFELOCKER.'/hacktor/release.log')));
+      $version = $data[1];
+    } else {
+      $data = explode('::', rtrim(file_get_contents(CMS_SAFELOCKER.'/hacktor/release.log')));
+      $version = $data[1];
+      $source = 'cache';
+    }
+
+    $output->writeln(sprintf('Metin2CMS <info>(%s)</info> version <comment>%s</comment>', ucfirst($source), $version));
   });
 $console
   ->register('repo:push')
@@ -72,7 +65,7 @@ $console
     $git = $wrapper->workingCopy(CMS_ROOT);
     $git->config('push.default', 'matching');
     $git->add('*')->commit($message)->push();
-    $output->writeln('<info>Repository update complete.</info>');
+    $output->writeln("<info>Repository update complete.</info>\nNew Commit: <comment>%s</comment>", $wrapper->git('rev-parse HEAD'));
 
     if ($cmsinfo['repository'] == 'nightly') {
       $version = $wrapper->git('rev-parse --short HEAD');
@@ -100,9 +93,6 @@ $console
   });
 $console
   ->register('repo:pull')
-  ->setDefinition(array(
-    // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
-  ))
   ->setDescription('Fetch changes from remote Git repository.')
   ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
     $wrapper = new GitWrapper();
