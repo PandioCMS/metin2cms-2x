8bd749367ca1bdde7df75e9960036fe5fc3de72f
Petru <petru.office92@gmail.com>
2015-08-17 15:03:04 +0200

    Added version logging after commit

--- backend/src/console.php
+++ backend/src/console.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use GitWrapper\GitWrapper;
-use GitWrapper\GitException;
 
 $cmsinfo = [
   'repository' => rtrim(file_get_contents(__DIR__.'/../safelocker/release')),
@@ -38,40 +37,29 @@ $console
   ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
     $message = ($input->getOption('message')) ? $input->getOption('message') : 'Repository update from Metin2CMS CLI.';
     $wrapper = new GitWrapper();
-    try {
+    $wrapper->setTimeout(3600);
+    $git = $wrapper->workingCopy(CMS_ROOT);
+    $git->config('push.default', 'matching');
+    $git->add('*')->commit($message)->push();
+    $repoType = file_get_contents(CMS_SAFELOCKER.'/release');
 
-      $wrapper->setTimeout(3600);
-      $git = $wrapper->workingCopy(CMS_ROOT);
-      $git->config('push.default', 'matching');
-      $git->add('*')->commit($message)->push();
-
-      $output->writeIn($wrapper->streamOutput());
-
-      $repoType = file_get_contents(CMS_SAFELOCKER.'/release');
-
-      if ($repoType == 'nightly') {
-        $version = $wrapper->git('rev-parse --short HEAD');
-      } else if($repoType == 'stable') {
-        $version = $wrapper->git('describe --exact-match --abbrev=0');
-      } else {
-        $version = 'unknown';
-      }
-
-      $logFile = CMS_SAFELOCKER.'/hacktor/release.log';
-      $fs = new Filesystem();
-      try {
-        if ($fs->exists($logFile)) {
-          file_put_contents($logFile, sprintf('%s::%s', $repoType, $version));
-        } else {
-          $fs->touch($logFile);
-          file_put_contents($logFile, sprintf('%s::%s', $repoType, $version));
-        }
-      } catch (IOExceptionInterface $e) {
-        $output->writeIn(sprintf('<error>Caught IOExceptionInterface: %s</error>', $e->getMessage()));
-      }
-
-    } catch (GitException $e) {
-      $output->writeIn(sprintf('<error>Caught GitException: %s</error>', $e->getMessage()));
+    if ($repoType == 'nightly') {
+      $version = $wrapper->git('rev-parse --short HEAD');
+    } else if($repoType == 'stable') {
+      $version = $wrapper->git('describe --exact-match --abbrev=0');
+    } else {
+      $version = 'unknown';
     }
+
+    $fs = new Filesystem();
+    $logFile = CMS_SAFELOCKER.'/hacktor/release.log';
+    if ($fs->exists($logFile)) {
+      file_put_contents($logFile, sprintf('%s::%s', $repoType, $version));
+    } else {
+      $fs->touch($logFile);
+      file_put_contents($logFile, sprintf('%s::%s', $repoType, $version));
+    }
+
+    $output->writeIn($wrapper->streamOutput());
   });
 return $console;
